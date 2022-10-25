7eec2fb02092e6767a9fe06c5e85f8abc876d074
Georgios Tzanakis <g.tzanakis@ferryhopper.com>
2022-10-25 13:01:16 +0300

    Rearranged code for readability

--- main.py
+++ main.py
@@ -19,8 +19,7 @@ ID_MAX = 2103353
 
 logging.basicConfig(level=logging.INFO)
 
-
-# Configure MySQL
+# MySQL
 
 # Alternatively use uri:
 # db_info = f"mysql://{user}:{password}@{hostname}/{database}"
@@ -33,8 +32,17 @@ db_info_mysql = {
     "host": os.environ["MYSQL_HOST"],
 }
 
+table_mysql_bookings = connect_to_table(
+    db_info_mysql,
+    table_name="reservations_bookings",
+    key_column="id",
+    min_key=ID_MIN,
+    max_key=ID_MAX,
+    where="bookingState='BOOKED'",
+)
 
-# Configure BigQuery
+
+# BigQuery
 
 credentials_gcloud = Credentials.from_service_account_file(
     filename=GOOGLE_KEY_PATH, scopes=["https://www.googleapis.com/auth/bigquery"]
@@ -47,15 +55,6 @@ db_info_bigquery = {
     "project": os.environ["BQ_PROJECT_ID"],
     "dataset": BQ_DATASET,
 }
-
-table_mysql_bookings = connect_to_table(
-    db_info_mysql,
-    table_name="reservations_bookings",
-    key_column="id",
-    min_key=ID_MIN,
-    max_key=ID_MAX,
-    where="bookingState='BOOKED'",
-)
 
 table_bigquery = connect_to_table(
     db_info_bigquery,
@@ -74,7 +73,7 @@ def main():
         plus_or_minus, columns = different_row
         print(plus_or_minus, columns)
 
-    print(time.time() - start)
+    print(f"Elapsed seconds: {time.time() - start}")
 
 
 if __name__ == "__main__":
